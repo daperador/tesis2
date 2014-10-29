9d107162ad6776d1e310077708231faaee9c070d
Daniel Aperador <danielaperador@gmail.com>
2014-10-28 23:24:35 -0500

    ajuste mtc para incluir el modelo de mvc y el de uml

--- workspace/co.edu.uniandes.csw.spl.generator/src/co/edu/uniandes/ticsw/spl/command/GenerateSPL.java
+++ workspace/co.edu.uniandes.csw.spl.generator/src/co/edu/uniandes/ticsw/spl/command/GenerateSPL.java
@@ -5,8 +5,15 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Properties;
+
+import mvc.Component;
+import mvc.Entities;
+import mvc.MVCModel;
+import mvc.Model;
+import mvc.impl.MvcFactoryImpl;
 
 import org.apache.commons.io.FileUtils;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -19,7 +26,10 @@ import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
+import org.eclipse.emf.ecore.resource.URIConverter.WriteableOutputStream;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
@@ -31,9 +41,13 @@ import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.dialogs.ElementListSelectionDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.EcoreUtil2;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.resource.XtextResourceSet;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Version;
 
+import co.edu.uniandes.csw.spl.dsl.MVCDslStandaloneSetup;
 import co.edu.uniandes.ticsw.spl.entity.ProductInfo;
 import co.edu.uniandes.ticsw.spl.mtcflow.standalone.MTCStandaloneUtils;
 import co.edu.uniandes.ticsw.spl.ui.ComponentSelectionManager;
@@ -41,7 +55,11 @@ import co.edu.uniandes.ticsw.spl.ui.GenerationDialog;
 import co.edu.uniandes.ticsw.spl.ui.ProductConfigurationDialog;
 import co.edu.uniandes.ticsw.spl.util.ProjectUtils;
 
+import com.google.inject.Injector;
 import com.mtcflow.engine.core.MTCChoreographer;
+import com.mtcflow.model.mtc.impl.MTCImpl;
+import com.mtcflow.model.mtc.impl.MtcFactoryImpl;
+import com.mtcflow.model.mtc.impl.MtcPackageImpl;
 
 public class GenerateSPL implements IHandler {
 
@@ -70,8 +88,8 @@ public class GenerateSPL implements IHandler {
 		final File originalModelFile = new File(ecoreResource.getLocation()
 				.toOSString());
 		try {
-			tempFile = File.createTempFile("spl", ".mvc");
-			FileUtils.copyFile(originalModelFile, tempFile);
+			tempFile = File.createTempFile("spl", ".xmi");
+			//FileUtils.copyFile(originalModelFile, tempFile);
 		} catch (IOException e) {
 			MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", "An error has occurred while creating temp file of the model");
 			throw new ExecutionException("An error has occurred while creating temp file of the model");
@@ -83,18 +101,63 @@ public class GenerateSPL implements IHandler {
 		Display.getDefault().asyncExec(new Runnable() {
 
 			public void run() {
+				Injector injector = new MVCDslStandaloneSetup().createInjectorAndDoEMFRegistration();
+				XtextResourceSet resourceSet = injector.getInstance(XtextResourceSet.class);
+				resourceSet.addLoadOption(XtextResource.OPTION_RESOLVE_ALL, Boolean.TRUE);
+				Resource resource3 = resourceSet.getResource(
+					    URI.createFileURI("C:\\tesis\\runtime-New_configuration\\PruebasDSL\\validaciones.annotateit"), true);
+				Resource resource4 = resourceSet.getResource(
+					    URI.createFileURI("C:\\tesis\\runtime-New_configuration\\PruebasDSL\\prueba.entities"), true);
+				Resource resource2=resourceSet.getResource(
+					    URI.createFileURI("C:\\tesis\\runtime-New_configuration\\PruebasDSL\\prueba.component"), true);
+				Resource resource1 = resourceSet.getResource(
+					    URI.createFileURI(originalModelFile.getAbsolutePath()), true);
+					
+				MVCModel model = (MVCModel) resource1.getContents().get(0);
+				
+				MVCModel model1 = EcoreUtil.copy(model);
+				model1.setEntities((Entities) resource4.getContents().get(0));
+				System.out.println(model.getComponents().get(0).getName());;
+				
+				
+				
+				for(Component component:model.getComponents()){
+					System.out.println(component.getName());
+					for(Model m:component.getModels()){
+						System.out.println(m.getName());
+						System.out.println(m.getRootEntity());
+						System.out.println(m.getRootEntity().getName());
+					}
+				}
+				
+				 
 				ResourceSet rset = new ResourceSetImpl();
+				File fi=new File(originalModelFile.getAbsolutePath()+".xmi");
+				try {
+					fi.createNewFile();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
 				Resource resource = rset.createResource(URI
 						.createFileURI(finalTempFile.getAbsolutePath()));
-				try {
-					resource.load(null);
-				} catch (IOException e) {
-					MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", "An error has occurred while reading model UML file");
+//				try {
+//					resource.load(null);
+//				} catch (IOException e) {
+//					MessageDialog.openError(Display.getDefault().getActiveShell(), "Error", "An error has occurred while reading model UML file");
+//					e.printStackTrace();
+//					throw new RuntimeException("An error has occurred while reading model UML file");
+//				}
+				try{
+					System.out.println(finalTempFile.getAbsolutePath());
+					resource.getContents().add(model1);
+					resource.save(Collections.EMPTY_MAP);
+				}catch(Exception e){
 					e.printStackTrace();
-					throw new RuntimeException("An error has occurred while reading model UML file");
 				}
+				
 				ComponentSelectionManager manager = new ComponentSelectionManager(
 						originalModelFile.getParentFile(), resource);
+				
 				ProductInfo selectedProduct = null;
 				if (action.equals("configureBasicProduct")) {
 					File configurationFile = new File(originalModelFile
@@ -202,7 +265,7 @@ public class GenerateSPL implements IHandler {
 					variables.put("tempModel", finalTempFile.getAbsolutePath());
 					// variables.put("outputWeb",
 					// ProjectUtils.workspaceDir.getAbsolutePath());
-					variables.put("tempModel", finalTempFile.getAbsolutePath());
+					//variables.put("tempModel", originalModelFile.getAbsolutePath());
 					variables.put("selectedClasses",
 							selectedProduct.getSelectedClasses());
 					variables.put("selectedAssociations",
@@ -217,6 +280,7 @@ public class GenerateSPL implements IHandler {
 					variables.put("splRepository",
 							selectedProduct.getSplRepository());
 					variables.put("plugginVersion", version.toString());
+					com.mtcflow.model.mtc.Model mod=MtcFactoryImpl.eINSTANCE.createModel();
 					final MTCChoreographer choreographer = MTCStandaloneUtils
 							.startMTC(ecoreResource.getProject(),
 									"co.edu.csw.spl.generator", "default.mtc",
